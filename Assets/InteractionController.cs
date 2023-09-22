adf2353ac8f65ed9874da0a67022f876d1b24142
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-09-22 11:39:30 +0200

    Controller

---
+++ Assets/InteractionController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InteractionController : MonoBehaviour
+{
+
+  [SerializeField]
+  GameObject bulletPrefab;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+    ActionBasedController controller = GetComponent<ActionBasedController>();
+
+    controller.activateAction.action.started += OnTrigger;
+
+  }
+
+  void OnTrigger(InputAction.CallbackContext ctx)
+  {
+    Debug.Log("BANG!");
+    Instantiate(bulletPrefab, transform.position, transform.rotation);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
